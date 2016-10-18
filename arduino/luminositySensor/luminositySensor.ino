b150cf93ff40f8907b8ee5b83e21629b9f8c1438
Rodrigo Buhler <rodrigobuhler@MacBookPro.local>
2016-10-18 13:36:33 -0200

    Arduino sketch updated - creation of a JSON output

--- arduino/luminositySensor/luminositySensor.ino
+++ arduino/luminositySensor/luminositySensor.ino
@@ -1,66 +1,99 @@
+#include <Time.h>
+#include <ArduinoJson.h>
 
 // Hardware Config
-int analogPin=0;    //no pino 0 para o Módulo Sensor de Luz.
+int nLumiSensor=0;    //pin 0 : Luminosity sensor
 
-int ledVerde=8;     //ledVerde no pino digital 8 do Arduino.
-int ledAmarelo=9;   //ledAmarelo no pino digital 9 do Arduino.
-int ledVermelho=10; //ledVermelho no pino digital 13 do Arduino.
+int nGreenLED=8;    //digital pin  8 : GrenLED
+int nYellowLED=9;   //digital pin  9 : YellowLED
+int nRedLED=10;     //digital pin 13 : RedLED
 
-int valAnalog;      //variável para armazenar o valor analógico
-                    
-int tensao;         //variável para armazenar o valor da tensão
+int nAnalogRead;    //analog buffer
 
-// Device Hana IoT SetUp
-String sAccount  = "p1941020166trial";
-String sDevice   = "9e8f52bf-281c-4fbc-9a34-9cd8e58710b5";
-String sToken    = "31479766523918bd1ef473b1d1a8b1a2";
-String sMsgType  = "ba2434ef5c2b4bc8de83";
-String sProxy    = "";
-
-String sSensorId = "ArdoLumy";
+// JSON header message
+String sAccount;
+String sDevice;
+String sToken;
+String sMsgType;
+String sProxy;
+int sUser;
+// JSON body message
+String sSensorId;
+int nRead;
+int dTimestamp;
 
 void setup()
 {
-Serial.begin(9600); //serial à uma velocidade de 9600bps(baud).
+Serial.begin(9600); //serial port speed : 9600bps(baud).
 
- pinMode(ledVerde, OUTPUT);   //configurando o pino8(ledVerde) como uma SAÍDA digital.
- pinMode(ledAmarelo, OUTPUT);   //configurando o pino9(ledAmarelo) como uma SAÍDA digital.
- pinMode(ledVermelho, OUTPUT);  //configurando o pino10(ledVermelho) como uma SAÍDA digital.
+  pinMode(nGreenLED, OUTPUT);    //pin setup as digital output
+  pinMode(nYellowLED, OUTPUT);   //pin setup as digital output
+  pinMode(nRedLED, OUTPUT);      //pin setup as digital output
  
- digitalWrite(ledVerde, LOW);     //Apaga o LED Verde.
- digitalWrite(ledAmarelo, LOW);   //Apaga o LED Amarelo.
- digitalWrite(ledVermelho, HIGH);  //Liga o LED Vermelho.
+  digitalWrite(nGreenLED, LOW);  //LED switched off
+  digitalWrite(nYellowLED, LOW); //LED switched off
+  digitalWrite(nRedLED, LOW);    //LED switched off
+
+  sAccount  = "p1941020166trial";
+  sDevice   = "9e8f52bf-281c-4fbc-9a34-9cd8e58710b5";
+  sToken    = "31479766523918bd1ef473b1d1a8b1a2";
+  sMsgType  = "ba2434ef5c2b4bc8de83";
+  sProxy    = "";
+  sUser     = 0x0;
+
+  sSensorId = "ArdoLumy";
+  nRead        = 0;
+  dTimestamp   = 0;
+ 
 }
 
 void loop()
 {
- valAnalog = analogRead(analogPin); //Lê o pino de entrada analógico zero (A0).
  /*
- Para converter os valores lidos pelo Módulo em tensão, deve-se
- ler o valor do sensor LDR do Módulo conectado na porta analógica
- do Arduino, cuja leitura varia de 0 a 1023, divide-se essa
- faixa em 5 seções, isso resultará no mapeamento do intervalo
- de 0V a 5V.
- */
- tensao = map(valAnalog, 0, 1023, 0, 5);
- Serial.println(tensao);
+ In order to convert the data read from the analog tension module 
+ connected to the Arduino it makes necessary a little calculation procedure.
  
- if (tensao < 2) { 
-  digitalWrite(ledVerde, HIGH); //Acende o LED Verde.
-  digitalWrite(ledAmarelo, LOW); //Apaga o LED Amarelo.
-  digitalWrite(ledVermelho, LOW); //Apaga o LED Vermelho.
+ The value range read from the module varies from 0 up to 1023, this
+ way dividing by 5 we have intervals of 0v to 5v (volts).
+ 
+ */
+ nAnalogRead = analogRead(nLumiSensor);   // Reads the Luminosity sensor
+ nRead = map(nAnalogRead, 0, 1023, 0, 5); // Calculates the intervals
+
+// Step 1: Reserve memory space
+  StaticJsonBuffer<400> jsonBuffer;
+
+// Step 2: Build object tree in memory
+  JsonObject& jRead = jsonBuffer.createObject();
+    jRead["sensor"] = sSensorId;
+    jRead["value"] = "nRead";
+    jRead["timestamp"] = 123456;
   
+  JsonObject& jMessage = jsonBuffer.createObject();
+    jMessage["account"]  = sAccount;
+    jMessage["device"]   = sDevice;
+    jMessage["devToken"] = sToken;
+    jMessage["messType"] = sMsgType;
+    jMessage["proxy"]    = sProxy;
+  JsonArray& messages = jMessage.createNestedArray("messages");
+    messages.add(jRead);
+
+  JsonObject& jArgs = jsonBuffer.createObject();
+  JsonArray& args = jArgs.createNestedArray("args");
+    args.add(jMessage);
+
+// Step 3: Generate the JSON string
+  jArgs.printTo(Serial);
+
+ if (nRead < 2) { 
+  trafficLightGreen();
  }else
-  if(tensao < 4)
+  if(nRead < 4)
  {
-  digitalWrite(ledAmarelo, HIGH); //Acende o LED Amarelo.
-  digitalWrite(ledVerde, HIGH); //Apaga o LED Verde.
-  digitalWrite(ledVermelho, LOW); //Apaga o LED Vermelho.
+  trafficLightYellow();
   
  }else{
-  digitalWrite(ledVermelho, HIGH); //Acende o LED Vermelho.
-  digitalWrite(ledVerde, HIGH); //Apaga o LED Verde.
-  digitalWrite(ledAmarelo, HIGH); //Apaga o LED Amarelo.
+  trafficLightRed();
   
  }
 }
@@ -68,21 +101,21 @@ void loop()
 // LIBRARY
 void trafficLightGreen()
 {
-  digitalWrite(ledVerde, HIGH);
-  digitalWrite(ledAmarelo, LOW);
-  digitalWrite(ledVermelho, LOW); 
+  digitalWrite(nGreenLED, HIGH);
+  digitalWrite(nYellowLED, LOW);
+  digitalWrite(nRedLED, LOW); 
 }
 
 void trafficLightYellow()
 {
-  digitalWrite(ledVerde, LOW);
-  digitalWrite(ledAmarelo, HIGH);
-  digitalWrite(ledVermelho, LOW); 
+  digitalWrite(nGreenLED, LOW);
+  digitalWrite(nYellowLED, HIGH);
+  digitalWrite(nRedLED, LOW); 
 }
 
-void trafficLightYellow()
+void trafficLightRed()
 {
-  digitalWrite(ledVerde, LOW);
-  digitalWrite(ledAmarelo, LOW);
-  digitalWrite(ledVermelho, HIGH); 
+  digitalWrite(nGreenLED, LOW);
+  digitalWrite(nYellowLED, LOW);
+  digitalWrite(nRedLED, HIGH); 
 }
